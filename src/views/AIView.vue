0378ef1d99a1f606f95b574be145c6512ecb6053
Nick <nnick1n@gmail.com>
2024-05-24 02:07:50 -0500

    made it always be in landscape

--- src/views/AIView.vue
+++ src/views/AIView.vue
@@ -5,7 +5,6 @@ const CARDS = [...Array(10).keys()]
 const cards = reactive(CARDS)
 const index = ref(-1)
 const current = computed(() => cards[index.value])
-const progress = computed(() => `width: ${((index.value + 1) / cards.length) * 100}%`)
 
 function click(val: number) {
   if (val === -1 && index.value === -1) {
@@ -30,37 +29,41 @@ function shuffle(array: number[]) {
 </script>
 
 <template>
-  <div class="h-screen w-screen">
-    <div class="fixed left-0 top-0 z-0 h-screen w-screen bg-white text-center text-6xl font-bold" :class="{ 'z-10': index === -1 }">
+  <div
+    class="fixed left-0 h-svh w-svw origin-top-left overflow-hidden text-center text-[5vmax] font-bold text-gray-800 portrait:top-full portrait:h-[100svw] portrait:w-[100svh] portrait:-rotate-90"
+  >
+    <div class="text-shadow fixed bottom-0 left-0 right-0 top-0 z-0" :class="{ 'z-20': index === -1 }">
       Tap on screen to shuffle<br />
       or draw top card
     </div>
-    <img src="/img/card-00.png" class="fixed left-0 top-0 z-0 h-screen w-screen" :class="{ 'z-10': current === 0 }" />
-    <img src="/img/card-01.png" class="fixed left-0 top-0 z-0 h-screen w-screen" :class="{ 'z-10': current === 1 }" />
-    <img src="/img/card-02.png" class="fixed left-0 top-0 z-0 h-screen w-screen" :class="{ 'z-10': current === 2 }" />
-    <img src="/img/card-03.png" class="fixed left-0 top-0 z-0 h-screen w-screen" :class="{ 'z-10': current === 3 }" />
-    <img src="/img/card-04.png" class="fixed left-0 top-0 z-0 h-screen w-screen" :class="{ 'z-10': current === 4 }" />
-    <img src="/img/card-05.png" class="fixed left-0 top-0 z-0 h-screen w-screen" :class="{ 'z-10': current === 5 }" />
-    <img src="/img/card-06.png" class="fixed left-0 top-0 z-0 h-screen w-screen" :class="{ 'z-10': current === 6 }" />
-    <img src="/img/card-07.png" class="fixed left-0 top-0 z-0 h-screen w-screen" :class="{ 'z-10': current === 7 }" />
-    <img src="/img/card-08.png" class="fixed left-0 top-0 z-0 h-screen w-screen" :class="{ 'z-10': current === 8 }" />
-    <img src="/img/card-09.png" class="fixed left-0 top-0 z-0 h-screen w-screen" :class="{ 'z-10': current === 9 }" />
-    <div class="fixed left-0 top-0 z-20 bg-indigo-400 text-center text-sm font-bold text-white transition-all duration-500" :style="progress">
+    <div v-for="i in CARDS" :key="i" class="fixed bottom-0 left-0 right-0 top-0 z-0 flex items-center justify-center" :class="{ 'z-20': current === i }">
+      <img :src="`/img/card-0${i}.png`" class="max-h-full" />
+    </div>
+    <div class="fixed bottom-0 left-0 right-0 top-0 z-10 bg-slate-100"></div>
+    <div class="fixed left-0 top-0 z-30 bg-indigo-400 text-sm text-white transition-all duration-500" :style="`width: ${((index + 1) / cards.length) * 100}%`">
       {{ index === -1 ? '' : index + 1 }}
     </div>
-    <div class="fixed left-0 top-0 z-30 grid h-screen w-screen grid-cols-2">
-      <button
-        @click="click(-1)"
-        class="cursor-pointer select-none text-center text-5xl font-bold text-gray-800 opacity-10 transition-opacity duration-500 active:opacity-90 active:duration-0"
-      >
+    <div class="fixed bottom-0 left-0 right-0 top-0 z-40 grid grid-cols-2">
+      <button @click="click(-1)" class="text-shadow cursor-pointer select-none opacity-10 transition-opacity duration-500 active:opacity-90 active:duration-75">
         {{ index === -1 ? 'Shuffle' : 'Prev' }}
       </button>
-      <button
-        @click="click(1)"
-        class="cursor-pointer select-none text-center text-5xl font-bold text-gray-800 opacity-10 transition-opacity duration-500 active:opacity-90 active:duration-0"
-      >
+      <button @click="click(1)" class="text-shadow cursor-pointer select-none opacity-10 transition-opacity duration-500 active:opacity-90 active:duration-75">
         {{ index < cards.length - 1 ? 'Draw' : 'End' }}
       </button>
     </div>
   </div>
 </template>
+
+<style>
+.text-shadow {
+  text-shadow:
+    -2px -2px 0 #fff,
+    0 -2px 0 #fff,
+    0 2px 0 #fff,
+    2px -2px 0 #fff,
+    -2px 2px 0 #fff,
+    2px 0 0 #fff,
+    -2px 0 0 #fff,
+    2px 2px 0 #fff;
+}
+</style>
