218453ad839353fd8894a93ee19239b640ea11f4
Nick <nnick1n@gmail.com>
2025-01-25 04:07:04 -0600

    minor tweak to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,10 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <main class="text-center text-2xl">
-    <h1 class="mt-8">Player Aids:</h1>
+    <h1 class="mt-8">Game Aids:</h1>
     <div>
-      <RouterLink to="/ai" class="inline-block border-b-2 border-rose-500">Heat: Pedal to the Metal
+      <RouterLink to="/ai" class="inline-block border-b-2 border-rose-500">
+        Heat: Pedal to the Metal AI
       </RouterLink>
     </div>
     <div>
@@ -14,22 +15,26 @@ import { RouterLink } from 'vue-router'
     </div>
     <h1 class="mt-8">Reference Cards:</h1>
     <div>
-      <RouterLink to="/king" class="inline-block border-b-2 border-rose-500">The King's Dilemma
+      <RouterLink to="/king" class="inline-block border-b-2 border-rose-500">
+        The King's Dilemma
       </RouterLink>
     </div>
     <div>
-      <RouterLink to="/king-print" class="inline-block border-b-2 border-rose-500">The King's Dilemma
-        Printable</RouterLink>
+      <RouterLink to="/king-print" class="inline-block border-b-2 border-rose-500">
+        The King's Dilemma Printable
+      </RouterLink>
     </div>
     <div>
-      <RouterLink to="/monster" class="inline-block border-b-2 border-rose-500">Kingdom Death: Monster
+      <RouterLink to="/monster" class="inline-block border-b-2 border-rose-500">
+        Kingdom Death: Monster
       </RouterLink>
     </div>
     <h1 class="mt-8">Score Cards:</h1>
     <div>
       <RouterLink to="/ticket" class="inline-block border-b-2 border-rose-500 font-bold">
-        <span class="text-red-800" style="font-variant: small-caps">Ticket to Ride</span> <span
-          class="text-blue-800">1901</span>
+        <span class="text-red-800" style="font-variant: small-caps">Ticket to Ride</span>
+        {{ ' ' }}
+        <span class="text-blue-800">1901</span>
       </RouterLink>
     </div>
   </main>
